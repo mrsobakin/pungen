9ac6288ca14c6ef3c620afb72d82e86a3ab8034f
Sobakin <68982655+mrsobakin@users.noreply.github.com>
2021-07-13 05:35:12 +0500

    Made pungen possible to use as a library

--- pungen.py
+++ pungen.py
@@ -3,32 +3,8 @@ import random
 
 
 class UsernameGenerator:
-    def __init__(self):
-        # command feedback for various arguments
-        min_length, max_length = 0, 0  # quells PyCharm "might be referenced before assignment" warnings
-        try:
-            max_length = int(sys.argv[2])  # max_length equals the first argument
-        except IndexError:  # if there is no second variable, flag length as a fixed number
-            is_ranged = False
-        else:
-            is_ranged = True  # the length of usernames is ranged
-
-        try:
-            if sys.argv[1] == "help":  # if the first argument is "help"
-                print("Usage: \n"  # print the usage instructions
-                      "    pungen <username_length>\n"
-                      "    pungen <username_length_min> <username_length_max>\n"
-                      "Note: It is recommended to keep lengths under 20 for more realistic user names.")
-                sys.exit()
-            if is_ranged:
-                min_length = int(sys.argv[1])  # min_length equals the first argument
-            else:
-                length = int(sys.argv[1])  # length equals the first argument
-        except IndexError:  # if the user did not pass a length argument
-            print("No length specified, using default of 7.")
-            length = 7  # set length to default of 7
-
-        # initialize tuples
+    def __init__(self, min_length, max_length=None):
+        self.set_length(min_length, max_length)
 
         self.consonants = ('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n',
                            'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z')
@@ -41,48 +17,55 @@ class UsernameGenerator:
         self.double_cons = ("he", "re", "ti", "ti", "hi", "to", "ll", "tt", "nn", "pp", "th", "nd", "st", "qu")
         self.double_vow = ("ee", "oo", "ei", "ou", "ai", "ea", "an", "er", "in",
                            "on", "at", "es", "en", "of", "ed", "or", "as")
+    
+    def set_length(self, min_length, max_length=None):
+        self.min_length = min_length
+        self.max_length = max_length
+    
+    def generate(self):
+        username, is_double, num_length = "", False, 0  # reset variables
+        
+        if random.randrange(10) > 0:
+            is_consonant = True
+        else:
+            is_consonant = False
 
-        # begin generating usernames
-        for i in range(10):
-            username, is_double, num_length = "", False, 0  # reset variables
-            if random.randrange(10) > 0:
-                is_consonant = True
-            else:
-                is_consonant = False
+        if self.max_length:  # pick a random length for this username given the ranges
+            length = random.randrange(self.min_length, self.max_length+1)
+        else:
+            length = self.min_length
 
-            if is_ranged:  # pick a random length for this username given the ranges
-                length = random.randrange(min_length, max_length)
+        if random.randrange(5) == 0:  # decide if there will be numbers after the name
+            num_length = random.randrange(3) + 1
+            if length - num_length < 2:  # we don't want the username to be too short
+                num_length = 0
 
-            if random.randrange(5) == 0:  # decide if there will be numbers after the name
-                num_length = random.randrange(3) + 1
-                if length - num_length < 2:  # we don't want the username to be too short
-                    num_length = 0
-
-            for j in range(length - num_length):  # we leave room for the numbers after the name here
-                if len(username) > 0:
-                    if username[-1] in self.consonants:
-                        is_consonant = False
-                    elif username[-1] in self.consonants:
-                        is_consonant = True
-                if not is_double:  # if the last character was a double, skip a letter
-                    # 1 in 8 chance of doubling if username is still short enough
-                    if random.randrange(8) == 0 and len(username) < int(length - num_length) - 1:
-                        is_double = True  # this character will be doubled
-                    if is_consonant:
-                        username += self.get_consonant(is_double)  # add consonant to username
-                    else:
-                        username += self.get_vowel(is_double)  # add vowel to username
-                    is_consonant = not is_consonant  # swap consonant/vowel value for next time
+        for j in range(length - num_length):  # we leave room for the numbers after the name here
+            if len(username) > 0:
+                if username[-1] in self.consonants:
+                    is_consonant = False
+                elif username[-1] in self.consonants:
+                    is_consonant = True
+            if not is_double:  # if the last character was a double, skip a letter
+                # 1 in 8 chance of doubling if username is still short enough
+                if random.randrange(8) == 0 and len(username) < int(length - num_length) - 1:
+                    is_double = True  # this character will be doubled
+                if is_consonant:
+                    username += self.get_consonant(is_double)  # add consonant to username
                 else:
-                    is_double = False  # reset double status so the next letter won't be skipped
-            if random.randrange(2) == 0:
-                # this was the best method I could find to only capitalize the first letter in Python 3
-                username = username[:1].upper() + username[1:]
-            if num_length > 0:
-                for j in range(num_length):  # loop 1 - 3 times
-                    username += str(random.randrange(10))  # append a random number, 0 - 9
-            print(username)
-
+                    username += self.get_vowel(is_double)  # add vowel to username
+                is_consonant = not is_consonant  # swap consonant/vowel value for next time
+            else:
+                is_double = False  # reset double status so the next letter won't be skipped
+        if random.randrange(2) == 0:
+            # this was the best method I could find to only capitalize the first letter in Python 3
+            username = username[:1].upper() + username[1:]
+        if num_length > 0:
+            for j in range(num_length):  # loop 1 - 3 times
+                username += str(random.randrange(10))  # append a random number, 0 - 9
+        
+        return username
+    
     def get_consonant(self, is_double):
         if is_double:
             return random.choice(self.double_cons)  # add two consonants from our pre-defined tuple
@@ -117,4 +100,25 @@ class UsernameGenerator:
             # return a random vowel based on the weight
             return self.vow_weighted[weight][random.randrange(len(self.vow_weighted[weight]))]
 
-UsernameGenerator()  # run the class
+if __name__ == "__main__":
+
+    argl = len(sys.argv) - 1
+    
+    if argl == 0:
+        min, max = 7, 13
+        count = 10
+    else:
+        if "-" in sys.argv[1]:
+            min, max = map(int, sys.argv[1].split("-"))
+        else:
+            min, max = int(sys.argv[1]), None
+        
+        if argl > 1:
+            count = int(sys.argv[2])
+        else:
+            count = 10
+    
+    username_generator = UsernameGenerator(min, max)
+    
+    for _ in range(count):
+        print(username_generator.generate())
