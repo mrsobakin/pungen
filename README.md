54250e98d498d29db0f5034eee319020525d32c4
mrsobakin <68982655+mrsobakin@users.noreply.github.com>
2023-02-04 21:14:04 +0500

    Refactor

--- README.md
+++ README.md
@@ -2,60 +2,54 @@
 Python Username Generator - for all your random username needs!
 
 
-pungen is a simple to use Python script which generates "usernames" on demand.
+pungen is a simple to use Python script which generates usernames on demand.
 The usernames are created on the spot using some pre-defined word-creating formulas.
 Don't worry. It's way less complicated than I'm making it sound.
 
 
-## Usage
+## How to use
 
 ### As a script
 
-The format for generating usernames from command line is:
+To generate usernames in the terminal, `cd` into the repository folder and use this command:
 
-`pungen.py [length // range] [count]`
+`python3 pungen.py [length / range] [count]`
 
 #### Example:
 ```console
-$ pungen.py
+$ python3 pungen.py
 Debezofotopp
-sanebusti9
-hidewenehiixo
-nerefida
-Dateter
-nodastoremo
-toppatefen
-rastari29
-Stitalirere
-canduzeretii
 
-$ pungen.py 5
-Nnote
+$ python3 pungen.py 5
 naral
-Hoo59
-coge8
-ta402
-Tuluz
-tures
-danuf
-modot
-tohia
 
-$ pungen.py 4-9 3
+$ python3 pungen.py 4-9 3
 Sasut379
 deseete
 toijomono
+
+$ python3 pungen.py 7-13 10
+baheimu
+Monuhot
+fenuhieheb
+tiutenenis
+hotito299
+Cituditofon
+Adipunanis7
+Harut722
+Nutilanu330
+Thondimefo
 ```
 
-### As a library
+### As a module
 
-You can also use pungen as a library. <sub><sup>*(Backward compatibility is not guaranteed)*</sub></sup>
+You can also use pungen as a module in your python code. <sub><sup>*(Though, backwards compatibility is not guaranteed)*</sub></sup>
 ```python
 from pungen import UsernameGenerator
 
-username_gen = UsernameGenerator(7, 13) # min, max = None
+username_gen = UsernameGenerator(7, 13)
 
 username = username_gen.generate()
 print(username)
-# Outputs sinoqanodo
+# sinoqanodo
 ```
--- pungen.py
+++ pungen.py
@@ -3,24 +3,25 @@ import random
 
 
 class UsernameGenerator:
+    CONSONANTS = "bcdfghjklmnpqrstvwxyz"
+
+    CONS_WEIGHTED = ("tn", "rshd", "lfcm", "gypwb", "vbjxq", "z")
+    VOW_WEIGHTED = ("eao", "iu")
+    DOUBLE_CONS = ("he", "re", "ti", "ti", "hi", "to", "ll", "tt", "nn", "pp", "th", "nd", "st", "qu")
+    DOUBLE_VOW = ("ee", "oo", "ei", "ou", "ai", "ea", "an", "er", "in", "on", "at", "es", "en", "of", "ed", "or", "as")
+
+
     def __init__(self, min_length, max_length=None):
         self.set_length(min_length, max_length)
 
-        self.consonants = ('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n',
-                           'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z')
-
-        self.vowels = ('a', 'e', 'i', 'o', 'u')
-
-        self.cons_weighted = (("t", "n"), ("r", "s", "h", "d"), ("l", "f", "c", "m"), ("g", "y", "p", "w", "b"),
-                              ("v", "b", "j", "x", "q"), "z")
-        self.vow_weighted = (("e", "a", "o"), ("i", "u"))
-        self.double_cons = ("he", "re", "ti", "ti", "hi", "to", "ll", "tt", "nn", "pp", "th", "nd", "st", "qu")
-        self.double_vow = ("ee", "oo", "ei", "ou", "ai", "ea", "an", "er", "in",
-                           "on", "at", "es", "en", "of", "ed", "or", "as")
     
-    def set_length(self, min_length, max_length=None):
+    def set_length(self, min_length, max_length):
+        if not max_length:
+            max_length = min_length
+            
         self.min_length = min_length
         self.max_length = max_length
+    
     
     def generate(self):
         username, is_double, num_length = "", False, 0  # reset variables
@@ -30,10 +31,8 @@ class UsernameGenerator:
         else:
             is_consonant = False
 
-        if self.max_length:  # pick a random length for this username given the ranges
-            length = random.randrange(self.min_length, self.max_length+1)
-        else:
-            length = self.min_length
+        # pick a random length for this username given the ranges
+        length = random.randrange(self.min_length, self.max_length+1)
 
         if random.randrange(5) == 0:  # decide if there will be numbers after the name
             num_length = random.randrange(3) + 1
@@ -42,18 +41,18 @@ class UsernameGenerator:
 
         for j in range(length - num_length):  # we leave room for the numbers after the name here
             if len(username) > 0:
-                if username[-1] in self.consonants:
+                if username[-1] in self.CONSONANTS:
                     is_consonant = False
-                elif username[-1] in self.consonants:
+                elif username[-1] in self.CONSONANTS:
                     is_consonant = True
             if not is_double:  # if the last character was a double, skip a letter
                 # 1 in 8 chance of doubling if username is still short enough
                 if random.randrange(8) == 0 and len(username) < int(length - num_length) - 1:
                     is_double = True  # this character will be doubled
                 if is_consonant:
-                    username += self.get_consonant(is_double)  # add consonant to username
+                    username += self._get_consonant(is_double)  # add consonant to username
                 else:
-                    username += self.get_vowel(is_double)  # add vowel to username
+                    username += self._get_vowel(is_double)  # add vowel to username
                 is_consonant = not is_consonant  # swap consonant/vowel value for next time
             else:
                 is_double = False  # reset double status so the next letter won't be skipped
@@ -66,9 +65,10 @@ class UsernameGenerator:
         
         return username
     
-    def get_consonant(self, is_double):
+    
+    def _get_consonant(self, is_double):
         if is_double:
-            return random.choice(self.double_cons)  # add two consonants from our pre-defined tuple
+            return random.choice(self.DOUBLE_CONS)  # add two consonants from our pre-defined tuple
         else:
             # we're just guessing at some good weights here. This is how more common letters get used more
             i = random.randrange(100)
@@ -84,13 +84,14 @@ class UsernameGenerator:
                 weight = 4
             else:
                 # the last group is Z by itself. No point in going through extra code when we can finish it here
-                return self.cons_weighted[5]
+                return self.CONS_WEIGHTED[5]
             # return a random consonant based on the weight
-            return self.cons_weighted[weight][random.randrange(len(self.cons_weighted[weight]))]
+            return self.CONS_WEIGHTED[weight][random.randrange(len(self.CONS_WEIGHTED[weight]))]
 
-    def get_vowel(self, is_double):
+
+    def _get_vowel(self, is_double):
         if is_double:
-            return random.choice(self.double_vow)  # add two vowels from our pre-defined tuple
+            return random.choice(self.DOUBLE_VOW)  # add two vowels from our pre-defined tuple
         else:
             i = random.randrange(100)
             if i < 70:
@@ -98,27 +99,25 @@ class UsernameGenerator:
             else:
                 weight = 1
             # return a random vowel based on the weight
-            return self.vow_weighted[weight][random.randrange(len(self.vow_weighted[weight]))]
+            return self.VOW_WEIGHTED[weight][random.randrange(len(self.VOW_WEIGHTED[weight]))]
+
 
 if __name__ == "__main__":
-
-    argl = len(sys.argv) - 1
-    
-    if argl == 0:
-        min, max = 7, 13
-        count = 10
+    if len(sys.argv) == 1:
+        min_length, max_length = 7, 13
     else:
         if "-" in sys.argv[1]:
-            min, max = map(int, sys.argv[1].split("-"))
+            min_length, max_length = map(int, sys.argv[1].split("-"))
         else:
-            min, max = int(sys.argv[1]), None
+            min_length = int(sys.argv[1])
+            max_length = min_length
         
-        if argl > 1:
-            count = int(sys.argv[2])
-        else:
-            count = 10
+    if len(sys.argv) >= 3:
+        count = int(sys.argv[2])
+    else:
+        count = 1
     
-    username_generator = UsernameGenerator(min, max)
+    pungen = UsernameGenerator(min_length, max_length)
     
     for _ in range(count):
-        print(username_generator.generate())
+        print(pungen.generate())
